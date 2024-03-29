8c8b36063462036afa41aed53d9bd82e7c6a06a5
svitlana-horodylova <horodylova.sv@gmail.com>
2024-03-29 10:23:40 +0000

    Added images and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../src/components/Layout/Layout';
 
-import { AppWrapper } from './App.styled';
-
 
 function App() {
   return (
-    <AppWrapper>
+    
       <Routes>
         <Route path="/" element={<Layout />}>
         </Route>
       </Routes>
-    </AppWrapper>
+   
   );
 }
 export default App;
