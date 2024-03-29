1de02e7e8f185fcca37ff45d173c7bc8053e3e9a
svitlana-horodylova <horodylova.sv@gmail.com>
2024-03-29 06:54:01 +0000

    Added App logic and some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import SharedLayout from 'components/SharedLayout/SharedLayout';
-import FirstPage from 'pages/FirstPage/FirstPage';
-import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import Layout from '../src/components/Layout/Layout';
+
 import { AppWrapper } from './App.styled';
 
-const test = import.meta.env.VITE_API_TEST;
 
 function App() {
-  console.log(test);
   return (
     <AppWrapper>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<Layout />}>
         </Route>
       </Routes>
     </AppWrapper>
