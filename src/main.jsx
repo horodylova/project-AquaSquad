854de0ae34097aa84ae6c26e6ee1e7439e1ea71e
svitlana-horodylova <horodylova.sv@gmail.com>
2024-03-29 11:13:00 +0000

    Added redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
+import store, { persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
+    <Provider store={store}>
+    <PersistGate persistor={persistor}> 
     <BrowserRouter basename="/project-AquaSquad">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+    </PersistGate>
+  </Provider>
+  
 );
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  username: null,
+  password: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUsername(state, action) {
+      state.username = action.payload;
+    },
+    setPassword(state, action) {
+      state.password = action.payload;
+    },
+  },
+});
+
+export const { setUsername, setPassword } = authSlice.actions;
+
+export default authSlice.reducer;
