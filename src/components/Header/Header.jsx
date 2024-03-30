50f1a9a66bd3642db652a8a1f67e062bfb2c54b6
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-03-29 20:34:01 -0700

    added logic header/layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,19 @@ import { HeaderLogo } from './HeaderLogo/HeaderLogo';
 import { UserAuth } from './UserAuth/UserAuth';
 import {UserLogo} from './UserLogo/UserLogo';
 // import { UserLogoModal } from '../Modals/HeaderModals/UserLogoModal/UserLogoModal';
-import { UserLogoutModal } from '../AllModals/UserLogoutModal/UserLogoutModal';
+// import { UserLogoutModal } from '../AllModals/UserLogoutModal/UserLogoutModal';
 
 
 
 
 
-export const Header = () => {
+export const Header = ({ isAuthenticated }) => {
   return (
     <Container>
       <HeaderContainer>
         <Navigation>
-          <HeaderLogo />
-          <UserAuth />
-          <UserLogo />
-          {/* <UserLogoModal/> */}
-          <UserLogoutModal/>
+          <HeaderLogo isAuthenticated={isAuthenticated} />
+        {isAuthenticated ? <UserLogo /> : <UserAuth />}
         </Navigation>
       </HeaderContainer>
     </Container>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,14 +3,21 @@ import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
 
 const SharedLayout = () => {
+  
+    // const isLoggedIn = useSelector(selectUserToken); // const selectUserToken = state => state.auth.token;
+
+
   return (
     <>
-      <Header />
+      {/* <Header  /> // isAuthenticated={isLoggedIn} */}
+      <Header/>
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
     </>
+  
   );
+    
 };
 
 export default SharedLayout;
