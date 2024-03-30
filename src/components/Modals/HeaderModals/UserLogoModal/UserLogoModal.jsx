f1d20d8ce63e4fc28ea8dd8935f786202adca66f
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-03-29 17:23:48 -0700

    updated path for imports header

--- src/components/Modals/HeaderModals/UserLogoModal/UserLogoModal.jsx
+++ src/components/Modals/HeaderModals/UserLogoModal/UserLogoModal.jsx
@@ -1,35 +1,27 @@
 import wheel from '../../../../Icons/settings.svg';
 import logout from '../../../../Icons/arrow-logout.svg';
-import { LogoModalMenu, ModalMenuIcon,ModalMenuBtn } from '../../UserLogoModal.styled';
+import {
+  LogoModalMenu,
+  ModalMenuIcon,
+  ModalMenuBtn,
+} from '../.././HeaderModals/UserLogoModal/UserLogoModal.styled';
 
 export const UserLogoModal = () => {
-  
   return (
-    
-      <LogoModalMenu>
-          
-          <ModalMenuBtn >
-            <ModalMenuIcon>
-            
-                <use href={wheel + '#icon-settings'}></use>
-              
-            </ModalMenuIcon>
-            Setting
-          </ModalMenuBtn>
+    <LogoModalMenu>
+      <ModalMenuBtn>
+        <ModalMenuIcon>
+          <use href={wheel + '#icon-settings'}></use>
+        </ModalMenuIcon>
+        Setting
+      </ModalMenuBtn>
 
-          <ModalMenuBtn >
-            <ModalMenuIcon>
-              
-                <use href={logout + '#icon-arrow-logout'}></use>
-              
-            </ModalMenuIcon>
-            Log out
-          </ModalMenuBtn>
-
-        </LogoModalMenu>
-      
-
+      <ModalMenuBtn>
+        <ModalMenuIcon>
+          <use href={logout + '#icon-arrow-logout'}></use>
+        </ModalMenuIcon>
+        Log out
+      </ModalMenuBtn>
+    </LogoModalMenu>
   );
 };
-
-
