35e84061d96f7e1c3436a289c3cd738e8a1a5b23
vosob <osobskyi.vlad@gmail.com>
2024-03-31 21:24:56 +0300

    fix auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Loader from '../src/components/Loader/Loader';
 import { PublicRoute } from '../src/Routes/PublicRoute/PublicRoute';
 
 // import { Route, Routes } from 'react-router-dom';
-// import { lazy, Suspense } from 'react';
-// import Layout from '../src/components/Layout/Layout';
-// import Loader from '../src/components/Loader/Loader';
-// import Modal from './components/AllModals/Modal';
+// import { PublicRoute } from './PublicRoute';
+
 // import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
+// const RegistrationPage = lazy(() =>
+//   import('./pages/RegistrationPage/RegistrationPage')
+// );
+// const LogInPage = lazy(() => import('../src/pages/LogInPage/LogInPage'));
 
 function App() {
   return (
@@ -18,17 +20,24 @@ function App() {
       <Layout>
         <PublicRoute path="/" component={WelcomePage} />
       </Layout>
+      {/* here */}
+      {/* <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute redirectRoute={'/'} restricted>
+                <LogInPage />
+              </PublicRoute>
+            }
+          />
+        </Route>
+      </Routes> */}
+      {/* here */}
+      {/* </Layout> */}
     </Suspense>
-    //   <Layout>
-    //     <Modal />
-    //     <Routes>
-    //       <Route path="/">
-    //         <Route index element={<WelcomePage />} />
-    //         <Route path="register" element={<RegistrationPage />} />
-    //       </Route>
-    //     </Routes>
-    //   </Layout>
-    // </Suspense>
   );
 }
 export default App;
--- src/components/Header/UserAuth/UserAuth.jsx
+++ src/components/Header/UserAuth/UserAuth.jsx
@@ -3,11 +3,17 @@ import user from '../../../Icons/user_outline.svg';
 
 export const UserAuth = () => (
   <RouterLink to="/signin">
-      Sign in
-      <HeaderIcon>
-        <svg>
-          <use href={user + '#icon-outline'}></use>
-        </svg>
-      </HeaderIcon>
-    </RouterLink>
+    Sign in
+    <HeaderIcon>
+      <svg>
+        <use href={user + '#icon-outline'}></use>
+      </svg>
+    </HeaderIcon>
+  </RouterLink>
+  // <RouterLink to="/login">
+  //   Sign in
+  //   <HeaderIcon>
+  //     <use href={user + '#icon-outline'}></use>
+  //   </HeaderIcon>
+  // </RouterLink>
 );
