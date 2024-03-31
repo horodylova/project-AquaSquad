2129e54d7c93707ed5d64a5e4f478c377ed9dd4e
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-03-30 20:59:14 -0700

    added header responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 
-import Container from '../Container/Container';
-import { HeaderContainer, Navigation } from './Header.styled';
+// import Container from '../Container/Container';
+import { HeaderContainer, Navigation, NavigationWrapper } from './Header.styled';
 import { HeaderLogo } from './HeaderLogo/HeaderLogo';
 import { UserAuth } from './UserAuth/UserAuth';
 import { UserLogo } from './UserLogo/UserLogo';
-import { SettingModal } from '../AllModals/SettingModal/SettingModal';
+// import { SettingModal } from '../AllModals/SettingModal/SettingModal';
 
 
 
@@ -14,14 +14,16 @@ import { SettingModal } from '../AllModals/SettingModal/SettingModal';
 
 export const Header = ({ isAuthenticated }) => {
   return (
-    <Container>
-      <HeaderContainer>
+    // <Container>
+    <HeaderContainer>
+      <NavigationWrapper>
         <Navigation>
           <HeaderLogo isAuthenticated={isAuthenticated} />
           {isAuthenticated ? <UserLogo /> : <UserAuth />}
         </Navigation>
-        <SettingModal/>
+        </NavigationWrapper>
+        {/* <SettingModal/> */}
       </HeaderContainer>
-    </Container>
+    // </Container>
   );
 };
