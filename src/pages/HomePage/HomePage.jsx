8fcd4fb08aecf345336c5611eabb421145f117c3
vosob <osobskyi.vlad@gmail.com>
2024-04-04 17:07:54 +0300

    f

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,44 @@
 import Container from '../../components/Container/Container';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/authOperations';
 import { WaterRatioPanel } from '../../components/HomePage/WaterRatioPanel/WaterRatioPanel';
 
 const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
+
+  const customStyles = {
+    content: {
+      width: '1008px',
+      height: '592px',
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      transform: 'translate(-50%, -50%)',
+      paddingTop: '32px',
+      paddingBottom: '32px',
+      paddingLeft: '24px',
+      paddingRight: '24px',
+    },
+    overlay: {
+      position: 'fixed',
+      backgroundColor: 'rgba(0, 0, 0, 0.80)',
+    },
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  Modal.setAppElement('#root');
 
   return (
     <>
@@ -15,6 +48,20 @@ const HomePage = () => {
         <button onClick={() => dispatch(logOut())} type="button">
           Logout
         </button>
+
+        <button onClick={openModal}>Open modal</button>
+
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h1>Setting</h1>
+          <p>Your photo</p>
+
+          <button onClick={closeModal}>close</button>
+        </Modal>
       </Container>
     </>
   );
