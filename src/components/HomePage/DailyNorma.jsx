e7df3c44dba9c0055cb6b617a527dd64b64e91ed
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2024-04-05 15:10:42 +0300

    Update DailyNorma.jsx

--- src/components/HomePage/DailyNorma.jsx
+++ src/components/HomePage/DailyNorma.jsx
@@ -5,13 +5,13 @@ import { Button, Text, TextWater, Wrap, BtnWrap } from "./DailyNorma.styled.js";
 export const DailyNorma = () => {
     const dailyNorma = useSelector(state => state.auth.user.dailyNorma);
    
-    const water = (dailyNorma / 1000).toFixed(1)
+    const water = dailyNorma ? (dailyNorma / 1000).toFixed(1) : "0";
     
     return (
         <Wrap>
             <Text>My daily norma</Text>
             <BtnWrap>
-                <TextWater>{water} L</TextWater>
+                <TextWater>{water || 0} L</TextWater>
                 <Button type="button"><span>Edit</span></Button>
             </BtnWrap>
         </Wrap>
