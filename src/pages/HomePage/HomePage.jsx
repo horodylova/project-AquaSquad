da52f8ecc5985587f96629ea9dd857c680adb32a
svitlana-horodylova <horodylova.sv@gmail.com>
2024-04-06 23:20:34 +0100

    Added containers

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,28 +1,28 @@
 import Container from '../../components/Container/Container';
 
-
 import { DailyNorma } from '../../components/HomePage/DailyNorma/DailyNorma';
 import { WaterRatioPanel } from '../../components/HomePage/WaterRatioPanel/WaterRatioPanel';
 import { Calendar } from '../../components/HomePage/Calendar/Calendar';
 import { TodayList } from '../../components/HomePage/TodayList/TodayList';
 
-import { BackGroundContainer } from './HomePage.styled';
-
+import { BackGroundContainer, ContentWrapper, LeftSideContainer, RightSideContainer } from './HomePage.styled';
 
 const HomePage = () => {
-  
- 
-
   return (
     <>
-    <BackGroundContainer>
-      <Container>
+      <BackGroundContainer>
+        <Container>
+          <ContentWrapper>
+            <LeftSideContainer>
             <DailyNorma />
             <WaterRatioPanel />
+            </LeftSideContainer>
+            <RightSideContainer>
             <TodayList />
-            <Calendar/>
-       
-      </Container>
+            <Calendar />
+            </RightSideContainer>
+          </ContentWrapper>
+        </Container>
       </BackGroundContainer>
     </>
   );
