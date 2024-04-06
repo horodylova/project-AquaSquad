d72163bba1075bce4220cca5f70ffe8301877130
vosob <osobskyi.vlad@gmail.com>
2024-04-07 01:06:14 +0300

    add DeleteEntryModal

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,10 @@
 import Container from '../../components/Container/Container';
-
 import { DailyNorma } from '../../components/HomePage/DailyNorma/DailyNorma';
 import { WaterRatioPanel } from '../../components/HomePage/WaterRatioPanel/WaterRatioPanel';
 import { Calendar } from '../../components/HomePage/Calendar/Calendar';
 import { TodayList } from '../../components/HomePage/TodayList/TodayList';
-
 import { BackGroundContainer } from './HomePage.styled';
+import DeleteEntryModal from '../../components/AllModals/DeleteEntryModal/DeleteEntryModal';
 
 const HomePage = () => {
   return (
@@ -16,6 +15,7 @@ const HomePage = () => {
           <WaterRatioPanel />
           <TodayList />
           <Calendar />
+          <DeleteEntryModal />
         </Container>
       </BackGroundContainer>
     </>
