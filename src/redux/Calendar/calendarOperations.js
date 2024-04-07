a14aa95b3b03797c3469a327b44eee5acc6f8df3
vosob <osobskyi.vlad@gmail.com>
2024-04-08 00:28:55 +0300

    fix get

--- src/redux/Calendar/calendarOperations.js
+++ src/redux/Calendar/calendarOperations.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 
 export const getDayWater = createAsyncThunk('today', async (_, thunkAPI) => {
   try {
-    const res = await axios.get('/calendar/today');
+    const res = await axios.post('/calendar/today');
     return res.data;
   } catch (error) {
     toast.error('Error get water for this day:', error);
@@ -12,17 +12,15 @@ export const getDayWater = createAsyncThunk('today', async (_, thunkAPI) => {
   }
 });
 
-export const getMonthWater = createAsyncThunk('month', async (_, thunkAPI) => {
-  try {
-    const res = await axios.get('/calendar/month', {
-      data: {
-        month: '04',
-        year: '2024',
-      },
-    });
-    return res.data;
-  } catch (error) {
-    toast.error('Error get water for this month:', error);
-    return thunkAPI.rejectWithValue(error.response.data.message);
+export const getMonthWater = createAsyncThunk(
+  'month',
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post('/calendar/month', credentials);
+      return res.data;
+    } catch (error) {
+      toast.error('Error get water for this month:', error);
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
   }
-});
+);
