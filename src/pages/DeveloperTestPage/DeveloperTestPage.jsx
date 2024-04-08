a24109c8ce3b57d1a51bc4b263ef62a330b28588
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2024-04-08 16:41:40 +0300

    edit and today

--- src/pages/DeveloperTestPage/DeveloperTestPage.jsx
+++ src/pages/DeveloperTestPage/DeveloperTestPage.jsx
@@ -1,7 +1,5 @@
 import Container from '../../components/Container/Container';
-
 import { TodayList } from '../../components/HomePage/TodayList/TodayList';
-
 import { useState } from 'react';
 import Modal from 'react-modal';
 import EditModal from '../../components/AllModals/EditModal/EditModal';
@@ -10,8 +8,11 @@ Modal.setAppElement('#root');
 
 export const DeveloperTestPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [waterId, setWaterId] = useState(0);
 
-  const handleEditbutton = () => {
+  const testId = '333';
+  const handleEditbutton = (id) => {
+    setWaterId(id);
     setIsModalOpen(true);
   };
 
@@ -19,17 +20,19 @@ export const DeveloperTestPage = () => {
     <Container>
       <p>Your test</p>
       <TodayList />
-      <button onClick={handleEditbutton}>
+      <button onClick={() => handleEditbutton(testId)}>
         Edit water
         <svg width="16" height="16">
           <use href="/src/Images/welcome-page/iconSprite.svg#icon-pencil"></use>
         </svg>
       </button>
-
-      <EditModal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-      />
+      {isModalOpen && (
+        <EditModal
+          isOpen={isModalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
+          waterId={waterId}
+        />
+      )}
     </Container>
   );
 };
