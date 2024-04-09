00b6b919a0f3298879cd2b2763f5178847c7ab91
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2024-04-09 09:35:03 -0700

    corrected main logo

--- src/components/Header/HeaderLogo/HeaderLogo.styled.jsx
+++ src/components/Header/HeaderLogo/HeaderLogo.styled.jsx
@@ -1,22 +1,9 @@
 import styled from 'styled-components';
 
 
-export const LogoIcon = styled.svg`
-  width: 40px;
-  height: 48px;
-`;
-
-export const Logo = styled.div`
+export const ContainerOfLogo = styled.div`
   display: flex;
   align-items: center;
-  color: #407BFF; 
-  font-size: 12px;
-  font-weight: 700;
-  text-transform: uppercase;
-  gap: 4px;
-`;
-
-export const LogoText = styled.span`
-  width: 58px;
-  height: 28px;
+  width: 102px;
+  height: 48px;
 `;
