5d1976f1de3448db8078d0e76756155c01af2705
svitlana-horodylova <horodylova.sv@gmail.com>
2024-04-11 14:30:36 +0100

    Update WaterRatioPanel.jsx

--- src/components/HomePage/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/HomePage/WaterRatioPanel/WaterRatioPanel.jsx
@@ -28,7 +28,6 @@ export const WaterRatioPanel = React.memo(() => {
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
-    console.log(open);
   };
 
   const handleCloseModal = () => {
